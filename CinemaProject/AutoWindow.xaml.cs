7032d1ab26f6be5dbb70f5cc6adb191892b16a95
Влуд Губарев <lolgagr@gmail.com>
2023-06-15 20:46:37 +0500

    Сделан стиль в виде KinoHub

---
+++ CinemaProject/AutoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CinemaProject
+{
+    /// <summary>
+    /// Логика взаимодействия для AutoWindow.xaml
+    /// </summary>
+    public partial class AutoWindow : Window
+    {
+        public AutoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- CinemaProject/MainWindow.xaml.cs
+++ CinemaProject/MainWindow.xaml.cs
@@ -12,17 +12,82 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using CinemaProject.View.Pages;
 
-namespace CinemaProject
+namespace PopupApp
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.NavigationService.Navigate(new MainPage());
+        }
+
+        private void Home_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Home;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Главная страница";
+        }
+
+        private void Home_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Profile_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Profile;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "в";
+        }
+
+        private void Profile_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Settings_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Settings;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Казахстан";
+        }
+
+        private void Settings_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HomeClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new MainPage());
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DragMove();
         }
     }
 }
